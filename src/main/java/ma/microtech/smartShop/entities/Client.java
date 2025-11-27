8e8d8c6927446e980bcc04e430ec9ba9574c9ca0
mohammed elmostaine <mohammedelmostaine@gmail.com>
2025-11-27 08:30:00 +0100

    Add User and Client entities

---
+++ src/main/java/ma/microtech/smartShop/entities/Client.java
@@ -0,0 +1,45 @@
+package ma.microtech.smartShop.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import ma.microtech.smartShop.entities.enums.CustomerTier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "clients")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private CustomerTier tier = CustomerTier.BASIC;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer totalOrders = 0;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Double totalSpent = 0.0;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<Order> orders = new ArrayList<>();
+}
