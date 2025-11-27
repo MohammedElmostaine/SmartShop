8e22ca8ef10f7e3132aba8d2b2d70fbb1b010773
mohammed elmostaine <mohammedelmostaine@gmail.com>
2025-11-27 08:30:00 +0100

    Add User and Client entities

---
+++ src/main/java/ma/microtech/smartShop/entities/User.java
@@ -0,0 +1,34 @@
+package ma.microtech.smartShop.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import ma.microtech.smartShop.entities.enums.UserRole;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Client client;
+}
