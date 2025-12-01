d579989e443d2d0335449f5a1d8afbefa227214b
mohammed elmostaine <mohammedelmostaine@gmail.com>
2025-12-01 08:45:00 +0100

    Add exception handlers

---
+++ src/main/java/ma/microtech/smartShop/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package ma.microtech.smartShop.exceptions;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ma/microtech/smartShop/exceptions/ForbiddenException.java
@@ -0,0 +1,8 @@
+package ma.microtech.smartShop.exceptions;
+
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
